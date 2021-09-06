163eff5c8a4b16d047270c2036831c3fad2c404d
alsan <alsan.wong@gmail.com>
2021-09-06 16:13:12 +0800

    feat(server): http "/" request handling

--- server/main.go
+++ server/main.go
@@ -24,6 +24,11 @@ func grpcServer(listener net.Listener) {
 }
 
 func httpServer(listener net.Listener) {
+	// the handler HAVE TO BE registered before Serve() is called
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, world!")
+	})
+
 	if err := http.Serve(listener, nil); err != nil {
 		log.Fatalf("Unable to start http server: %v", err)
 	}
@@ -44,7 +49,7 @@ func main() {
 
 	// declare the match rules for different services requested
 	grpcFilter := tcpm.MatchWithWriters(cmux.HTTP2MatchHeaderFieldSendSettings("content-type", "application/grpc"))
-	httpFilter := tcpm.Match(cmux.Any())
+	httpFilter := tcpm.Match(cmux.HTTP1Fast())
 
 	// initialize the servers by passing in the custom listeners
 	go grpcServer(grpcFilter)
